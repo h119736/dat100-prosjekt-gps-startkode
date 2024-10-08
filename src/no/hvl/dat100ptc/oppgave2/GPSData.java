107aef90babd6733e7c355e6959dfcee676973ba
h119736 <119736@stud.hvl.no>
2024-10-08 13:56:38 +0200

    Update GPSData.java

--- src/no/hvl/dat100ptc/oppgave2/GPSData.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSData.java
@@ -39,39 +39,19 @@ public class GPSData {
 
 	public boolean insert(String time, String latitude, String longitude, String elevation) {
 
-		// Omgjør tekst til tall
+	
 		GPSPoint gpspoint = GPSDataConverter.convert(time, latitude, longitude, elevation);
 
 		
 
-		// bruk insert metode for å putte objektet i tabellen.
-
-		return insertGPS(gpspoint); //metodekallet virker ikke...
+		return insertGPS(gpspoint); 
 		
 
 	}
 
 	public void print() {
 
-		/*
-		 * public void print() som skriver ut GPS data som finnes i gpspoints-tabellen
-		 * på følgende formen
-		 * 
-		 * ====== GPS Data - START ====== 1 (1.0,2.0) 3.0 2 (4.0,5.0) 6.0 3 (7.0,8.0)
-		 * 9.0 ====== GPS Data - SLUTT ====== Hint: bruk løkke og toString-metoden på
-		 * GPSPoint-objekt.
-		 * 
-		 * Metodene som allerede er implementert i klassen GPSDataFileReader.java leser
-		 * - linje for linje - i GPS datafilen og lagrer data i tabellen ved å bruke
-		 * insert-metoden som ble implementert ovenfor.
-		 * 
-		 * Dette betyr at punktene i gpspoint-tabellen svarer til ruten som er
-		 * representert i GPS datafilen og gpspoint-referansetabellen vil ha samme
-		 * lengde som antallet av GPS punkter som er leste inn. Det siste betyr videre
-		 * at hvert element i gpspoint-tabellen vil peke på et objekt. dvs. tabellen er
-		 * full.
-		 * 
-		 */
+	
 		
 		System.out.println("====== GPS Data - START ======");
 		
