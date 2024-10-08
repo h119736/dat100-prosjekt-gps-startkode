c862f579af96bc6da54a8ce358ea9676f465096f
h119736 <119736@stud.hvl.no>
2024-10-08 13:56:47 +0200

    Ferdig

--- src/no/hvl/dat100ptc/oppgave2/Main.java
+++ src/no/hvl/dat100ptc/oppgave2/Main.java
@@ -4,10 +4,20 @@ import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
 public class Main {
 
-	
 	public static void main(String[] args) {
 
-		// TODO
+	
 		
+		GPSPoint p1 = new GPSPoint(4353, 54.44, 23.54, 23.33);
+		GPSPoint p2 = new GPSPoint (4325, 53.44, 65.34, 24.54);
+		
+		GPSData gpsData = new GPSData(2);
+		
+		
+		gpsData.insertGPS(p1);
+		gpsData.insertGPS(p2);
+		
+		gpsData.print();
+
 	}
 }
